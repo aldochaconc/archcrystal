bc4eb643d6a1fc7dd8e02adf70b42d5bc756a2bd
Aldo W. <aldochacon@gmail.com>
2024-01-28 04:36:10 -0300

    fix: missing wild card

--- ArchCrystal/0-preinstall.sh
+++ ArchCrystal/0-preinstall.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 echo -ne "
 -------------------------------------------------------------------------
                     0 - Pre-Install
@@ -115,11 +116,11 @@ echo -ne "
 "
 pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
 mkdir -p /mnt/root
-cp -R ${BASE_DIR}/ /mnt/root/
+cp -R ${BASE_DIR}/* /mnt/root/
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
 genfstab -L /mnt >>/mnt/etc/fstab
-echo " 
+echo "
   Generated /etc/fstab:
 "
 cat /mnt/etc/fstab
@@ -133,26 +134,3 @@ if [[ ! -d "/sys/firmware/efi" ]]; then
 else
     pacstrap /mnt efibootmgr --noconfirm --needed
 fi
-echo -ne "
--------------------------------------------------------------------------
-                    Checking for low memory systems <8G
--------------------------------------------------------------------------
-"
-TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
-if [[ $TOTAL_MEM -lt 8000000 ]]; then
-    # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
-    mkdir -p /mnt/opt/swap  # make a dir that we can apply NOCOW to to make it btrfs-friendly.
-    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
-    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
-    chmod 600 /mnt/opt/swap/swapfile # set permissions.
-    chown root /mnt/opt/swap/swapfile
-    mkswap /mnt/opt/swap/swapfile
-    swapon /mnt/opt/swap/swapfile
-    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
-    echo "/opt/swap/swapfile	none	swap	sw	0	0" >>/mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
-fi
-echo -ne "
--------------------------------------------------------------------------
-                    SYSTEM READY FOR 1-setup.sh
--------------------------------------------------------------------------
-"
