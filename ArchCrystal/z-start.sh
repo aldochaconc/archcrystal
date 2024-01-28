793d8625e289567008f1eed969a1d1967c9ace9f
Aldo W. <aldochacon@gmail.com>
2024-01-27 21:36:03 -0300

    fix: minor typo

--- ArchCrystal/z-start.sh
+++ ArchCrystal/z-start.sh
@@ -6,7 +6,7 @@ set +a
 
 (bash "$BASE_DIR"/y-environment.sh) |& tee startup.log
 source "$BASE_DIR/x-setup.conf"
-(bash "$SCRIPTS_DIR/0-preinstall.sh") |& tee 0-preinstall.log
+(bash "$BASE_DIR/0-preinstall.sh") |& tee 0-preinstall.log
 (arch-chroot /mnt $HOME/ArchCrystal/1-setup.sh) |& tee 1-setup.log
 (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchCrystal/2-user.sh) |& tee 2-user.log
 (arch-chroot /mnt $HOME/ArchCrystal/3-post-setup.sh) |& tee 3-post-setup.log
