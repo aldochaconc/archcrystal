6363368c7bed961121e76f2050d79a6442efa9b5
Aldo W. <aldochacon@gmail.com>
2024-02-28 02:53:58 -0300

    chore: remove unused logs

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -1,31 +1,15 @@
 #!/usr/bin/env bash
 
-echo -ne "
-
-Final Setup and  -SConfigurations
-GRUB EFI Bootloader Install & Check
-
-"
+echo "GRUB EFI Bootloader Install & Check"
 source ${HOME}/archcrystal/setup.conf
 
 if [[ -d "/sys/firmware/efi" ]]; then
   grub-install --efi-directory=/boot ${DISK}
 fi
 
-echo -ne "
-
-Creating (and Theming) Grub Boot Menu
-
-"
 echo -e "Updating grub..."
 grub-mkconfig -o /boot/grub/grub.cfg
-echo -e "All set!"
 
-echo -ne "
-
-Cleaning
-
-"
 # Remove no password sudo rights
 sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
@@ -33,5 +17,4 @@ sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: A
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# Replace in the same state
 cd $pwd
